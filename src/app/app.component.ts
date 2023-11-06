04bc3268bdcb24bc76e7fab77a5cf3db881d88de
yusukesuzukiFun <yusuke-suzuki@fundasta.co.jp>
2023-11-06 14:36:09 +0900

    テスト作成

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,32 +12,42 @@ export class AppComponent {
 
   constructor(private appService: AppService) {}
 
-  msg = '';
+  msg = 'test';
 
-  getDemoPerson(valueId: string): void {
-    this.appService
-      .getApi(valueId)
-      .subscribe(
-        (message) =>
-          (this.msg = `ID: ${message['body']['message']['person_id']['S']}, Name: ${message['body']['message']['name']['S']}`)
-      );
+  getDemoPerson(valueId: string): any {
+    try {
+      this.appService
+        .getApi(valueId)
+        .subscribe(
+          (message) =>
+            (this.msg = `ID: ${message['body']['message']['person_id']['S']}, Name: ${message['body']['message']['name']['S']}`)
+        );
+    } catch (error) {
+      this.msg = `Error: ${error}`;
+      // throw error;
+      throw new Error();
+    }
   }
 
-  postDemoPerson(valueId: string, valueName: string): void {
+  postDemoPerson(valueId: string, valueName: string): any {
     this.appService
       .postApi(valueId, valueName)
       .subscribe((message) => (this.msg = message['body']['message']));
   }
 
-  putDemoPerson(valueId: string, valueName: string): void {
+  putDemoPerson(valueId: string, valueName: string): any {
     this.appService
       .putApi(valueId, valueName)
       .subscribe((message) => (this.msg = message['body']['message']));
   }
 
-  deleteDemoPerson(valueId: string): void {
+  deleteDemoPerson(valueId: string): any {
     this.appService
       .deleteApi(valueId)
       .subscribe((message) => (this.msg = message['body']['message']));
+  }
+
+  testDemoPerson() {
+    console.log('test');
   }
 }
